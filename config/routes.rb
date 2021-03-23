747f1eb826b265e9c8258becd6133dafb22548de
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-23 02:17:23 +0000

    コントローラ・ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'propertys/index'
-  get 'propertys/new'
-  get 'propertys/edit'
-  get 'propertys/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :propertys
+
 end
